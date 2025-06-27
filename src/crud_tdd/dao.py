eaa942277232232e5e8c0c93423b72829fbcec56
Pamela Gutiérrez <ptgutierrezgm@gmail.com>
2025-06-27 16:54:02 -0400

    Implementación GREEN

--- src/crud_tdd/dao.py
+++ src/crud_tdd/dao.py
@@ -1,3 +1,4 @@
+# src/crud_tdd/dao.py
 
 from crud_tdd.models import Item
 from typing import List
@@ -14,3 +15,24 @@ class ItemDao:
 
     def delete(self, id: int) -> None:
         raise NotImplementedError
+
+class ItemDaoImpl(ItemDao):
+    def __init__(self):
+        self._almacen: List[Item] = []
+
+    def create(self, item: Item) -> None:
+        self._almacen.append(item)
+
+    def read_all(self) -> List[Item]:
+        return list(self._almacen)
+
+    def update(self, item: Item) -> None:
+        for idx, existente in enumerate(self._almacen):
+            if existente.id == item.id:
+                self._almacen[idx] = item
+                return
+        raise ValueError("Item no encontrado")
+
+    def delete(self, id: int) -> None:
+        self._almacen = [i for i in self._almacen if i.id != id]
+
--- tests/test_dao.py
+++ tests/test_dao.py
@@ -25,3 +25,20 @@ def test_delete_no_implementado():
     dao = DummyDao()
     with pytest.raises(NotImplementedError):
         dao.delete(1)
+
+def test_create_y_read_all():
+    dao = ItemDaoImpl()
+    dao.create(Item(1, "A"))
+    assert any(i.nombre == "A" for i in dao.read_all())
+
+def test_update_exitoso():
+    dao = ItemDaoImpl()
+    dao.create(Item(1, "Antes"))
+    dao.update(Item(1, "Después"))
+    assert dao.read_all()[0].nombre == "Después"
+
+def test_delete_exitoso():
+    dao = ItemDaoImpl()
+    dao.create(Item(1, "X"))
+    dao.delete(1)
+    assert dao.read_all() == []
